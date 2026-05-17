e0b10c2b90aab9f9f73d051d0aa205517ed3f33a
Missa <victor@maxperson.com.br>
2026-05-17 02:17:21 -0400

    fix: adicionado try catch para evitar contato preso on processing

--- app/Jobs/ContactScoreJob.php
+++ app/Jobs/ContactScoreJob.php
@@ -9,11 +9,13 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use App\Models\Contact as ContactModel;
 use App\Events\ContactScoreEvent;
+use Illuminate\Support\Facades\Log;
 
 use App\Domain\Contact\Services\ScoreCalculator;
 use App\Domain\Contact\ValueObjects\Nome;
 use App\Domain\Contact\ValueObjects\Email;
 use App\Domain\Contact\ValueObjects\Phone;
+use App\Domain\Contact\Repositories\ContactRepositoryInterface;
 
 class ContactScoreJob implements ShouldQueue
 {
@@ -26,33 +28,44 @@ class ContactScoreJob implements ShouldQueue
         $this->contactDatabaseId = $contactDatabaseId;
     }
 
-    public function handle(): void
+    public function handle(ContactRepositoryInterface $repository): void
     {
+        try {
+             $contactModel = ContactModel::find($this->contactDatabaseId);
 
-        $contactModel = ContactModel::find($this->contactDatabaseId);
+            if (!$contactModel) {
+                return; 
+            }
 
-        if (!$contactModel) {
-            return; 
-        }
+            //simula request/Work
+            sleep(2);
 
-        //simula request/Work
-        sleep(2);
+            $calculator = app(ScoreCalculator::class); 
 
-        $calculator = app(ScoreCalculator::class); 
+            $nomeObj  = new Nome($contactModel->name);
+            $emailObj = new Email($contactModel->email);
+            $phoneObj = new Phone($contactModel->phone);
 
-        $nomeObj  = new Nome($contactModel->name);
-        $emailObj = new Email($contactModel->email);
-        $phoneObj = new Phone($contactModel->phone);
+            $score = $calculator->calculate($nomeObj, $emailObj, $phoneObj);
 
-        $score = $calculator->calculate($nomeObj, $emailObj, $phoneObj);
-
-        $contactModel->update([
-            'score'  => $score,
-            'status' => 'active', 
-            'processed_at' => now(),
-        ]);
+            $contactModel->update([
+                'score'  => $score,
+                'status' => 'active', 
+                'processed_at' => now(),
+            ]);
 
         event(new ContactScoreEvent($contactModel->id, $score, 'active'));
-
+            
+        } catch (\Throwable $exception) {
+                        
+            Log::error("Erro no Job: " . $exception->getMessage());
+            
+            $contact = $repository->findById($this->databaseId); 
+            
+            if ($contact) {
+                $contact->failProcess();
+                $repository->save($contact);
+            }
+        }
     }
 }
