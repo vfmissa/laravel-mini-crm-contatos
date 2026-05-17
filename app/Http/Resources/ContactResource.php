6096674c78fd07d2dbf403ef64aa4c4202735094
Missa <victor@maxperson.com.br>
2026-05-17 00:14:02 -0400

    feat(api): form request validacao e api com regras de validacao

---
+++ app/Http/Resources/ContactResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ContactResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'           => $this->getDatabaseId(),
+            'name'         => $this->getName()->getValue(),
+            'email'        => $this->getEmail()->getValue(),
+            'phone'        => $this->getPhone()->getValue(),
+            'status'       => $this->getStatus()->value,
+            'score'        => $this->getScore(),
+        ];
+    }
+}
